51232415fada22e0ff90be4dc268f766311fba3b
EllieAioli <73043999+EllieAioli@users.noreply.github.com>
2020-10-17 19:12:17 -0500

    Add files via upload

--- files/scripts.js
+++ files/scripts.js
@@ -1,6 +1,8 @@
-var defaultMap = "files/df2map_10-16-20.jpg";
+
 var debug = 0;
 $(document).ready(function() {
+	var defaultMap = "files/df2map_10-16-20.jpg";
+	
 	function thousands_separators(num){
 	    var num_parts = num.toString().split(".");
 	    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
