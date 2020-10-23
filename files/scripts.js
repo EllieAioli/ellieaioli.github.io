18d9ccd527ca8df533c2475927a149ed52ac1d49
Lily Talley <aaron@LNet.local>
2020-10-23 15:12:12 -0500

    map

--- files/scripts.js
+++ files/scripts.js
@@ -1,9 +1,9 @@
 
 var debug = 0;
 $(document).ready(function() {
-	var defaultMap = "files/df2map_10-22-20.jpg";
+	var defaultMap = "files/df2map_10-16-20.jpg";
 	//test
-	
+
 	function thousands_separators(num){
 	    var num_parts = num.toString().split(".");
 	    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
